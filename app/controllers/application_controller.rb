c74578cdd3b303ea3e649576c100cb7479eb7624
Lucas-Jiang-9723 <yj1084@uchicago.edu>
2022-07-10 21:30:40 +0000

    first commit - 4_calc finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,46 @@
 class ApplicationController < ActionController::Base
+  def add
+    render({:template => "calculation/add_ask.html.erb"})
+  end
+
+  def subtract
+    render({:template => "calculation/subtract_ask.html.erb"})
+  end
+
+  def multiply
+    render({:template => "calculation/multiply_ask.html.erb"})
+  end
+
+  def divide
+    render({:template => "calculation/divide_ask.html.erb"})
+  end
+
+  def add_show
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num + @second_num
+    render({:template => "calculation/add_show.html.erb"})
+  end
+
+  def subtract_show
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = - @first_num + @second_num
+    render({:template => "calculation/subtract_show.html.erb"})
+  end
+
+  def multiply_show
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num * @second_num
+    render({:template => "calculation/multiply_show.html.erb"})
+  end
+  
+  def divide_show
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num / @second_num
+    render({:template => "calculation/divide_show.html.erb"})
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-
+  get("/add",{:controller => "application", :action => "add"})
+  get("/subtract",{:controller => "application", :action =>"subtract"})
+  get("/multiply",{:controller => "application", :action =>"multiply"})
+  get("/divide",{:controller => "application", :action =>"divide"})
+  get("/wizard_add",{:controller => "application", :action => "add_show"})
+  get("/wizard_subtract",{:controller => "application", :action => "subtract_show"})
+  get("/wizard_multiply",{:controller => "application", :action => "multiply_show"})
+  get("/wizard_divide",{:controller => "application", :action => "divide_show"})
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
